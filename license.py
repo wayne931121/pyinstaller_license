61784eba36ca69981d3854aad53af140911840fb
Wayne咏辰 <75261164+wayne931121@users.noreply.github.com>
2025-11-08 15:20:46 +0800

    Create license.py

---
+++ license.py
@@ -0,0 +1,30 @@
+"""
+Attribution 4.0 International
+Copyright (c) 2025 wayne931121
+Source https://github.com/wayne931121/
+
+Usage:
+python license.py basePath targetPath
+
+Example:
+python license.py "C:\Program Files\Python313" ".\0"
+"""
+
+import find, sys, os
+
+basePath = sys.argv[1]
+targetPath = sys.argv[2]
+
+def copy(basePath, targetPath, partten):
+    for i in find.find(partten,basePath,typeMode="file"):
+        newFile = i.replace("\\","-").replace("/","-").replace(":","-")+".txt"
+        f  = open(os.path.join(targetPath,newFile),"wb")
+        r  = open(os.path.join(basePath,i),"rb")
+        f.write(r.read())
+        r.close()
+        f.close()
+    return    
+
+copy(basePath, targetPath, "license")
+copy(basePath, targetPath, "License")
+copy(basePath, targetPath, "LICENSE")
